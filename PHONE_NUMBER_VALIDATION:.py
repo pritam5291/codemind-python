28f737a72472ddcf5bf8e3d36f4a6e000cbae649
Pritam Dandapat <119730085+pritam5291@users.noreply.github.com>
2023-05-24 14:59:50 +0530

    x=int(input()) c=0 r=0 while(x>0):     d=x%10     x=x//10     c+=1     r=r*10+d if(c==10):     k=r%10     r=r//10     if(k==0):         print("Invalid")     else:         print("Valid") else:     print("Invalid")

---
+++ PHONE_NUMBER_VALIDATION:.py
@@ -0,0 +1,17 @@
+x=int(input())
+c=0
+r=0
+while(x>0):
+    d=x%10
+    x=x//10
+    c+=1
+    r=r*10+d
+if(c==10):
+    k=r%10
+    r=r//10
+    if(k==0):
+        print("Invalid")
+    else:
+        print("Valid")
+else:
+    print("Invalid")
